10257a746aaa63b45a590bdc80e896046218dc9c
wickeym <wickeym@gmail.com>
2024-02-23 07:56:41 -0500

    FIX: Move favicon into public folder

--- components/SEO.js
+++ components/SEO.js
@@ -6,7 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
-      <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     </Head>
   );
 }
